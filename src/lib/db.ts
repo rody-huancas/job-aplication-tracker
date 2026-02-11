f3e27e67e2fcb1388c081ede19e880a6305bfd67
Rody Huancas <rodyhuancas.04@gmail.com>
2026-02-10 22:21:20 -0500

    Feat: enhance authentication module with improved error handling and validation for user input

---
+++ src/lib/db.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error(
+    "Por favor define el MONGODB_URI en las variables de entorno",
+  );
+}
+
+interface MongooseCache {
+  conn   : typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache | undefined;
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
+const connectDB = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = { bufferCommands: false };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+
+  return cached.conn;
+};
+
+export default connectDB;
